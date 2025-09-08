3916373e90def1cb223219a5a02ad31a4e18f6fa
Adithyaharivpn <adithyaharivpn@gmail.com>
2025-09-08 23:31:04 +0530

    new page

--- client/src/components/Review.jsx
+++ client/src/components/Review.jsx
@@ -1,26 +1,8 @@
-import { Box, Card, AspectRatio, Typography } from '@mui/material';
 import React from 'react'
 
 const Review = () => {
-    const data = [
-  {
-    src: 'https://images.unsplash.com/photo-1502657877623-f66bf489d236',
-    title: 'Night view',
-    description: '4.21M views',
-  },
-  {
-    src: 'https://images.unsplash.com/photo-1527549993586-dff825b37782',
-    title: 'Lake view',
-    description: '4.74M views',
-  },
-  {
-    src: 'https://images.unsplash.com/photo-1532614338840-ab30cf10ed36',
-    title: 'Mountain view',
-    description: '3.98M views',
-  },
-];
   return (
-    
+    <div>Review</div>
   )
 }
 
