2a9de2ee1e6198ee92045b79eb853641999776ef
Adithyaharivpn <adithyaharivpn@gmail.com>
2025-09-09 20:53:41 +0530

    fixed error

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,9 +10,9 @@ import Review from "./components/Review.jsx";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Footer from "./components/Footer.jsx";
-import Signup from "./components/Signup.jsx";
+import Signup from "./components/SignUp.jsx";
 import Login from "./components/Login.jsx";
-import {Routes,Route} from 'react-router-dom'
+import { Routes, Route } from "react-router-dom";
 function App() {
   const HomePage = () => (
     <>
@@ -28,7 +28,7 @@ function App() {
         <NavBar />
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/signup" element={<Signup />} />
+          <Route path="/signup" element={<SignUp />} />
           <Route path="/login" element={<Login />} />
         </Routes>
         <Footer />
