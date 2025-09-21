e8506cca3d02c083acd4de7fec4c4b47da089364
Adithyaharivpn <adithyaharivpn@gmail.com>
2025-09-21 23:20:47 +0530

    updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,7 +15,6 @@ import Footer from "./components/Homepage/Footer.jsx";
 import Login from "./components/Homepage/Login.jsx";
 import JobPosting from "./components/User/JobPosting.jsx";
 import JobsPage from "./components/User/JobsPage.jsx";
-import Test from "./components/Homepage/Test.jsx";
 import PopularJobs from "./components/Homepage/PopularJobs.jsx";
 function App() {
   const HomePage = () => (
